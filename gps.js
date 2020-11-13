40c07099b739c15d697609be71b66981e8ca82ea
Andrey Smirnov <ankingster@gmail.com>
2020-11-13 18:44:02 -0500

    Gps module connect-disconnect issues fixed

--- gps.js
+++ gps.js
@@ -3,21 +3,30 @@
 const GPS = require('gps')
 const net = require('net');
 const fs = require('fs')
-var rl = require('readline');
+//var rl = require('readline');
+const { exec } = require('child_process');
 
-
+// //Create objects
+// const serial = new SerialPort('/dev/ttyACM0', { baudRate: 115200 })
+const gps = new GPS;
 const socketName = 'Zedf9p';
-const gps = new GPS
 const path = './file.txt'
 
-// Check if receiving socket can be opened
+// Check if receiving socket for communication with f9p application can be opened
 try {
-  if (fs.existsSync(path)) {
-	//file exists/ socket cannot be opened
-	console.log('node receiving socket cannot be opened because temporary file that was created for it was never removed from /tmp/'+socketName+'.sock')
-  }
-} catch(err) {
-  console.error(err)
+	if (fs.existsSync('/tmp/' + socketName + '.sock')) {
+		//file exists/ socket cannot be opened
+		console.log('node receiving socket cannot be opened because temporary file that was created for it was never removed from /tmp/' + socketName + '.sock')
+		console.log('trying to remove it')
+		fs.unlinkSync('/tmp/' + socketName + '.sock')
+	} else {
+		console.log('no file interference, receive socket can be opened successfully')
+	}
+} catch (err) {
+	console.error(err)
+	console.log('unable to remove file, gps cannot be started')
+	//exitHandler()
+	process.exit();
 }
 
 
@@ -25,42 +34,37 @@ try {
 var server = net.createServer(socket => {
 	socket.on('data', c => {
 		//console.log(c.toString());
-
+		gps.updatePartial(c)
 	});
 
-	var i = rl.createInterface(socket, socket);
-	i.on('line', function (line) {
-		//socket.write(line);
-		console.log(line)
-		gps.updatePartial(line)
-	});
+	//read one line at a time interface
+	// var i = rl.createInterface(socket, socket);
+	// i.on('line', function (line) {
+	// 	//socket.write(line);
+	// 	console.log(line)
+	// 	//gps.updatePartial(line)
+	// 	gps.update(line)
+	// });	
 
 	socket.on('end', () => {
 		//server.close();
 	});
 });
 
-server.listen('/tmp/'+socketName+'.sock');
+//Start listening on a changed for this socket
+server.listen('/tmp/' + socketName + '.sock');
 
-//var stream = net.connect('/tmp/Zedf9p.sock');
-//stream.write('hello');
-//stream.end();
-
-// var path = require("path");
-
-// var server = net.createServer(function (client) {
-//     client.on('data', function (data) {
-//         console.log("data: " + data.toString());
-//     });
-// });
-
-// server.listen(path.join('\\\\.\\pipe', pipeName), function() {
-//     console.log("connected");
-// });
-
-// //Create objects
-// const serial = new SerialPort('/dev/ttyACM0', { baudRate: 115200 })
-// const gps = new GPS
+// Start F9p module reader application
+const childF9pProcess = exec('/home/pi/f9p/Zedf9p -com-port /dev/ttyACM0  -ntrip-server rtk2go.com -ntrip-password 6n9c2TxqKwuc -ntrip-port 2101 -ntrip-mountpoint Wexford -rtcm-accuracy-req 2.000 -rtcm-survey-time 60', (err, stdout, stderr) => {
+	if (err) {
+		//some err occurred
+		console.error(err)
+	} else {
+		// the *entire* stdout and stderr (buffered)
+		console.log(`stdout: ${stdout}`);
+		console.log(`stderr: ${stderr}`);
+	}
+});
 
 //GPS parser received data and processes it
 const gpsOutput = data => {
@@ -68,7 +72,7 @@ const gpsOutput = data => {
 		console.log(data.lat, data.lon)
 
 		//write message to parent caller
-		process.send({ lat: data.lat, lon: data.lon })
+		process.send && process.send({ lat: data.lat, lon: data.lon })
 	}
 }
 
@@ -90,7 +94,7 @@ process.on('message', msg => {
 	// if (msg === 'gps on') gps.on('data', gpsOutput)
 	// if (msg === 'rtcm') {
 	// 	console.log('rtcm signal received by gps module')
-		
+
 
 	// }
 })
@@ -99,23 +103,26 @@ process.on('message', msg => {
 //Do any socket cleanup before this app exits
 function exitHandler(options, exitCode) {
 	//if (options.cleanup) console.log('clean');
-	
+
+	//terminate child process
+	childF9pProcess && childF9pProcess.kill('SIGINT');
+
 	//close unix socket
 	server && server.close();
 
-    if (exitCode || exitCode === 0) console.log(exitCode);
-    if (options.exit) process.exit();
+	if (exitCode || exitCode === 0) console.log(exitCode);
+	if (options.exit) process.exit();
 }
 
 //do something when app is closing
-process.on('exit', exitHandler.bind(null,{cleanup:true}));
+process.on('exit', exitHandler.bind(null, { cleanup: true }));
 
 //catches ctrl+c event
-process.on('SIGINT', exitHandler.bind(null, {exit:true}));
+process.on('SIGINT', exitHandler.bind(null, { exit: true }));
 
 // catches "kill pid" (for example: nodemon restart)
-process.on('SIGUSR1', exitHandler.bind(null, {exit:true}));
-process.on('SIGUSR2', exitHandler.bind(null, {exit:true}));
+process.on('SIGUSR1', exitHandler.bind(null, { exit: true }));
+process.on('SIGUSR2', exitHandler.bind(null, { exit: true }));
 
 //catches uncaught exceptions
-process.on('uncaughtException', exitHandler.bind(null, {exit:true}));
+process.on('uncaughtException', exitHandler.bind(null, { exit: true }));
